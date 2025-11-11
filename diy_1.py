0f9dc6882b040aa4085dfaeb565399ff988a109c
niyati siju <143396643+niyatiiii28@users.noreply.github.com>
2025-11-11 17:41:46 +0530

    Add files via upload

---
+++ diy_1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 24 23:46:15 2025
+
+@author: niyatisiju
+"""
+''' Questin 1 
+name=input("enter your name:")
+print("hello, "+ name+ " welcome to this program")
+
+val1=int(input("enter value 1: "))
+val2=int(input("enter value 2: "))
+
+print("orignal value 1 : ", val1)
+print("orignal value 2 : ",  val2)
+
+va1,val2= val2, val1
+
+print("after swapping")
+print("value 1: ",val1)
+print("value 2: ", val2)'''
+
+
+''' question 2
+import math
+radius=float(input("enter the radius of the circle: "))
+area= math.pi * (radius **2)
+
+print("area of the circle: ", area) 
+'''
+
+'''question 3
+birth_year= input("enter your birth year:")
+if len(birth_year)<4 or int(birth_year)<0: 
+    print("not valid")
+    current_age=0
+else: 
+    current_age = 2025 - int(birth_year)
+
+print("your current age: ", current_age)
+'''
+
+'''question 4
+name=input("enter your name:")
+fav_cake= input("enter your fav cake flavour: ")
+
+print(f"Hello, {name}! We're delighted to serve you your favorite {fav_cake} cake on your birthday. Happy Birthday.")
+'''
+
+'''question5 
+
+P=int(input("enter principal amount: "))
+R=float(input("enter rate: "))
+N=int(input("enter time period: "))
+
+I= P * R * N /100 
+
+print("Intrest: ", I )'''
+
+
+
+
+
---
+++ lec2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 16 19:14:18 2025
+
+@author: niyatisiju
+"""
+
+'''
+
+file_path="/Users/niyatisiju/Pictures/Photos Library.photoslibrary/originals/0"
+
+print(file_path)
+
+name="niyati"
+age=18
+print("hello my name is {} and my age is {}".format(name,age))
+
+print(f"hello my name is {name} and i am {age} years old")
+
+'''
+
+name=input("enter your name:")
+check_in_date=input("date of check_in: ")
+check_out_date= input("date of check_out:")
+
